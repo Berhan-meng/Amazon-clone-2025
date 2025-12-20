ff1cbe2932a28cbeb93e9dd324a58c34998bf9fc
Berhan-meng <berhanumengesha3@gmail..com>
2025-12-20 06:46:32 +0300

    cart page up and down arrow style updated

--- Amazon-clone/src/main.jsx
+++ Amazon-clone/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./index.module.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { DataProvider } from "./assets/Components/DataProvider/DataProvider.jsx";
 import { reducer, initialState } from "./Utility/reducer.js";
-import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
   <DataProvider value={reducer} initialState={initialState}>
